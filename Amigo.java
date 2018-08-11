8ef96cd4744321ab722b5ba684fde8672d255b1c
peterkrauz <37553561+peterkrauz@users.noreply.github.com>
2018-08-10 21:06:05 -0300

    Create Amigo.java

---
+++ Amigo.java
@@ -0,0 +1,35 @@
+public class Amigo extends Pessoa{
+    String nome;
+    String email;
+    String fone;
+
+    public Amigo(String nome, String email, String fone) {
+        super(nome);
+        this.email = email;
+        this.fone = fone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+}
