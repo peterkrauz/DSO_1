77c2653afa3c9e799e523300cc8c10824652d2d3
peterkrauz <37553561+peterkrauz@users.noreply.github.com>
2018-08-10 21:54:19 -0300

    Update Programa.java

--- Programa.java
+++ Programa.java
@@ -1,32 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Programa {
     boolean rodando;
     Scanner scanner;
-    // Seu primo possui muitos Livros, todos em papel, e vive
-    //emprestando aos amigos dele. Entretanto, muitas vezes
-    //ele esquece a quem emprestou e já perdeu diversos
-    //Livros por causa disso. No almoço da família, no último
-    //final de semana, ele pediu para você desenvolver um
-    //programa para resolver este problema. Esse sistema
-    //deve permitir o cadastro dos amigos, incluindo: nome,
-    //número do telefone e e-mail e também cadastrar os
-    //Livros, com: título, resumo, autor, personagem principal,
-    //gênero (comédia, romance ou aventura, etc) e faixa
-    //etária. Deve ser possível cadastrar os empréstimos e
-    //obter a lista de Livros emprestados incluindo quais
-    //amigos estão com quais Livros.
+    Cadastrador cadastrador;
+    List<Amigo> amigos;
+    List<Livro> livros;
+    List<Emprestimo> emprestimos;
 
-    public void evaluateChoice(int escolha){
+    public void evaluateChoice(int escolha, Cadastrador cadastrador){
         switch (escolha){
             case 1:
                 System.out.println("opçao 1 escolhida");
+                amigos.add(cadastrador.cadastrarAmigo());
                 break;
             case 2:
                 System.out.println("opçao 2 escolhida");
+                livros.add(cadastrador.cadastrarLivro());
                 break;
             case 3:
                 System.out.println("opçao 3 escolhida");
+
+                System.out.println("Digite o nome do amigo...");
+                String nomeAmigo = scanner.nextLine();
+                scanner.nextLine();
+
+                System.out.println("Digite o titulo do livro...");
+                String tituloLivro = scanner.nextLine();
+
+                System.out.println("Digite a data de hoje...");
+                String data = scanner.nextLine();
+
+                Amigo amigo = acharAmigo(nomeAmigo);
+                Livro livro = acharLivro(tituloLivro);
+                emprestimos.add(cadastrador.cadastrarEmprestimo(amigo, livro, data));
                 break;
             case 4:
                 scanner.close();
@@ -36,12 +45,25 @@ public class Programa {
         }
     }
 
+    private Livro acharLivro(String tituloLivro) {
+        return new Livro();
+    }
+
+    private Amigo acharAmigo(String nomeAmigo) {
+        return new Amigo();
+    }
+
     public Programa(){
         this.rodando = true;
         scanner = new Scanner(System.in);
+        Cadastrador cadastrador = new Cadastrador();
+        amigos = new ArrayList<>();
+        livros = new ArrayList<>();
+        emprestimos = new ArrayList<>();
+
         while(rodando){
-            System.out.println("Selecione a opção... \n(1)Cadastrar Amigo\t(2)Cadastrar Livro\n(3)Ver livros emprestados\t(4)Sair ");
-            evaluateChoice(scanner.nextInt());
+            System.out.println("Selecione a opção...\n(1)Cadastrar Amigo\t(2)Cadastrar Livro\n(3)Ver livros emprestados\t(4)Sair ");
+            evaluateChoice(scanner.nextInt(), cadastrador);
         }
     }
 
