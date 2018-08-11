190a0c2bba3cf0e062738b8332a2644276945cb8
peterkrauz <37553561+peterkrauz@users.noreply.github.com>
2018-08-10 21:55:05 -0300

    Update Cadastrador.java

--- Cadastrador.java
+++ Cadastrador.java
@@ -1,4 +1,3 @@
-import java.util.Date;
 import java.util.Scanner;
 
 public class Cadastrador {
@@ -21,7 +20,27 @@ public class Cadastrador {
         return new Amigo(nome, email, fone);
     }
 
-    public Emprestimo cadastrarEmprestimo(Amigo amigo, Livro livro, Date date){
+    public Livro cadastrarLivro(){
+        scanner = new Scanner(System.in);
+
+        System.out.println("Digite o titulo do novo livro...");
+        String titulo = scanner.nextLine();
+        System.out.println("Digite o resumo do novo livro...");
+        String resumo = scanner.nextLine();
+        System.out.println("Digite o autor do novo livro...");
+        Autor autor = new Autor(scanner.nextLine());
+        System.out.println("Digite o personagemPrincipal do novo livro...");
+        Pessoa personagemPrincipal = new Pessoa(scanner.nextLine());
+        System.out.println("Digite o genero do novo livro...");
+        String genero = scanner.nextLine();
+        System.out.println("Digite o faixaEtaria do novo livro...");
+        String faixaEtaria = scanner.nextLine();
+
+        scanner.close();
+        return new Livro(titulo, resumo, autor, personagemPrincipal, genero, faixaEtaria);
+    }
+
+    public Emprestimo cadastrarEmprestimo(Amigo amigo, Livro livro, String date){
         return new Emprestimo(livro, amigo, date);
     }
 
