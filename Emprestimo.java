8b56777f0a084e186cedc6f4ad36de85bdd1342d
peterkrauz <37553561+peterkrauz@users.noreply.github.com>
2018-08-10 21:54:39 -0300

    Update Emprestimo.java

--- Emprestimo.java
+++ Emprestimo.java
@@ -1,16 +1,36 @@
-import java.util.Date;
-
 public class Emprestimo {
     Livro livro;
     Amigo amigo;
-    Date dataEfetuacao;
+    String dataEfetuacao;
 
-    public Emprestimo(Livro livro, Amigo amigo, Date dataEfetuacao) {
+    public Emprestimo(Livro livro, Amigo amigo, String dataEfetuacao) {
         this.livro = livro;
         livro.setEmprestado(true);
         this.amigo = amigo;
         this.dataEfetuacao = dataEfetuacao;
     }
 
+    public Livro getLivro() {
+        return livro;
+    }
 
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Amigo getAmigo() {
+        return amigo;
+    }
+
+    public void setAmigo(Amigo amigo) {
+        this.amigo = amigo;
+    }
+
+    public String getDataEfetuacao() {
+        return dataEfetuacao;
+    }
+
+    public void setDataEfetuacao(String dataEfetuacao) {
+        this.dataEfetuacao = dataEfetuacao;
+    }
 }
