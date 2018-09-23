3152e695f7997257fa8f8089cf077f2ade859b7a
peterkrauz <betinhocaimatando@gmail.com>
2018-09-23 19:12:06 -0300

    abcedfg
    aa caralho

--- ex07_elevador/Elevador.java
+++ ex07_elevador/Elevador.java
@@ -10,83 +10,92 @@
  */
 public class Elevador implements IElevador {
 
-  private int capacidade;
-  private int totalPessoas;
-  private int totalAndaresPredio;
-  private int andar;
+    private int capacidade;
+    private int totalPessoas;
+    private int totalAndaresPredio;
+    private int andar;
 
-  public Elevador(){}
+    public Elevador(){}
 
-  public Elevador(int andar, int totalAndaresPredio, int capacidade, int totalPessoas){
-    this.capacidade = capacidade;
-    this.totalPessoas = totalPessoas;
-    this.totalAndaresPredio = totalAndaresPredio;
-    this.andar = andar;
-  }
-
-  @Override
-  public int getCapacidade(){
-    return capacidade;
-  }
-  @Override
-  public int getTotalPessoas(){
-    return totalPessoas;
-  }
-  @Override
-  public String descer() throws ElevadorJahNoTerreoException{
-    if( andar > 0 ){
-      setAndar(andar - 1);
-    } else {
-      throw new ElevadorJahNoTerreoException("Elevador já no térreo");
+    public Elevador(int andar, int totalAndaresPredio, int capacidade, int totalPessoas){
+      this.capacidade = capacidade;
+      this.totalPessoas = totalPessoas;
+      this.totalAndaresPredio = totalAndaresPredio;
+      this.andar = andar;
     }
-    return "";
-  }
-  @Override
-  public String entraPessoa() throws ElevadorCheioException{
-    if ( getTotalPessoas() == capacidade ) {
-      throw new ElevadorCheioException("Elevador cheio.");
-    } else {
-      setTotalPessoas( totalPessoas + 1 );
-    }
-    return "";
-  }
-  @Override
-  public String saiPessoa() throws ElevadorJahVazioException{
-    if( getTotalPessoas() == 0  ){
-      throw new ElevadorJahVazioException();
-    } else {
-      setTotalPessoas( totalPessoas - 1 );
-    }
-    return "Pessoa saiu (?)";
-  }
-  @Override
-  public String subir() throws ElevadorJahNoUltimoAndarException{
-    if ( getAndarAtual() == totalAndaresPredio ) {
-      throw new ElevadorJahNoUltimoAndarException();
-    } else {
-      setAndar( andar + 1 );
-    }
-    return "Elevador subiu (?)";
-  }
-  @Override
-  public void setTotalAndaresPredio(int totalAndaresPredio){
-    this.totalAndaresPredio = totalAndaresPredio;
-  }
-  @Override
-  public int getTotalAndaresPredio(){
-    return totalAndaresPredio;
-  }
-  @Override
-  public int getAndarAtual(){
-    return andar;
-  }
 
-  public int setAndar(int novoAndar){
-    this.andar = novoAndar;
-  }
+    @Override
+    public int getCapacidade(){
+      return capacidade;
+    }
 
-  public int setTotalPessoas(int novoTotal){
-    this.totalPessoas = novoTotal;
-  }
+    @Override
+    public int getTotalPessoas(){
+      return totalPessoas;
+    }
+
+    @Override
+    public String descer() throws ElevadorJahNoTerreoException{
+      if( andar > 0 ){
+        setAndar(andar - 1);
+      } else {
+        throw new ElevadorJahNoTerreoException("Elevador já no térreo");
+      }
+      return "";
+    }
+
+    @Override
+    public String entraPessoa() throws ElevadorCheioException{
+      if ( getTotalPessoas() == capacidade ) {
+        throw new ElevadorCheioException("Elevador cheio.");
+      } else {
+        setTotalPessoas( totalPessoas + 1 );
+      }
+      return "";
+    }
+
+    @Override
+    public String saiPessoa() throws ElevadorJahVazioException{
+      if( getTotalPessoas() == 0  ){
+        throw new ElevadorJahVazioException();
+      } else {
+        setTotalPessoas( totalPessoas - 1 );
+      }
+      return "Pessoa saiu (?)";
+    }
+
+    @Override
+    public String subir() throws ElevadorJahNoUltimoAndarException{
+      if ( getAndarAtual() == totalAndaresPredio ) {
+        throw new ElevadorJahNoUltimoAndarException();
+      } else {
+        setAndar( andar + 1 );
+      }
+      return "Elevador subiu (?)";
+    }
+
+    @Override
+    public void setTotalAndaresPredio(int totalAndaresPredio){
+      this.totalAndaresPredio = totalAndaresPredio;
+    }
+
+    @Override
+    public int getTotalAndaresPredio(){
+      return totalAndaresPredio;
+    }
+
+    @Override
+    public int getAndarAtual(){
+      return andar;
+    }
+
+    public void setAndar(int novoAndar){
+      this.andar = novoAndar;
+      return novoAndar;
+    }
+
+    public void setTotalPessoas(int novoTotal){
+      this.totalPessoas = novoTotal;
+    }
 
 }
